ac09cd5622850d8f96c5d18b265f90f32b579451
AnataVortex7 <akshaymerat@gmail.com>
2025-08-29 20:22:34 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -10,7 +10,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", ""))
 CREDIT = environ.get("CREDIT", "𝘽𝙊𝙏𝙎")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7268596608').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
 AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
@@ -20,4 +20,5 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
